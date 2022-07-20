9259421c07ca57e324a8c46160f02fff86b2e735
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-20 09:37:11 +0530

    [Hitesh]: added calculating length of two lines and check equality

--- src/com/bridgelabz/LineComparison/LineComparison.java
+++ src/com/bridgelabz/LineComparison/LineComparison.java
@@ -11,17 +11,28 @@ public class LineComparison {
 	 */
 	public static void main(String[] args) {
 		/**
-		 * taking two coordinates to create a straight line
-		 * calculating its length
+		 * assigning four cordinates which will make two straight lines
+		 * calculating the distance of two lines
+		 * comparing the two lines are equal or not , using equalsTo method
+		 * 
 		 */
 		System.out.println("Welcome to Line Comparison computation");
 		int x1 = 3;
 		int x2 = 7;
 		int y1 = 4;
-		int y2 = 2;
-		double dis = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println("Two points are (x1,y1)(2,3) and (x2,y2)(4,1)");
-		System.out.println("Length of the line (2,3)(4,1) is " + dis);
+		int y2 = 3;
+		int a1 = 4;
+		int a2 = 9;
+		int b1 = 6;
+		int b2 = 5;
+	    double disOfFirstLine = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2));
+	    double disOfSecondLine = Math.sqrt(Math.pow((a2-a1), 2) + Math.pow((b2-b1), 2));
+	    System.out.println("Two points are (x1,y1)(3,4) and (x2,y2)(7,2)");
+	    System.out.println("Another Two points are (a1,b1)(4,6) and (a2,b2)(9,5)");
+	    System.out.println("Length of the first line (3,4)(7,2) is " + disOfFirstLine);
+	    System.out.println("Length of the second line (4,6)(9,5) is " + disOfSecondLine);
+	    Integer obj1 = new Integer((int) disOfFirstLine);
+	    Integer obj2 = new Integer((int) disOfSecondLine);
+	    System.out.println("these lines are equal :" + obj1.equals(obj2));
 	}
-
 }
